64dab4d871dd46f1e9e73c7b379672f7dee63ebe
Michael Bowman <bowmanmc@gmail.com>
2016-09-07 20:22:13 -0400

    initial version of map.js

--- tutorial/30_geojson/map.js
+++ tutorial/30_geojson/map.js
@@ -13,24 +13,15 @@ $(document).ready(function() {
     var countiesLayer = L.featureGroup();
     countiesLayer.addTo(mymap);
 
-    var bindPopup = function(feature, layer) {
-        var countyTemplate = '<h2>{COUNTY_NAM}</h2><p>FIPS Code: {FIPS_CODE}</p><p>2000 Population: {POP_2000}</p>';
-        layer.bindPopup(L.Util.template(countyTemplate, feature.properties));
-    };
-
     // First, we need to load the GeoJSON file
     $.get('counties.oh.json', function(data) {
 
-        // L.geoJSON takes a second argument for processing options. Here, we're
-        // telling Leaflet to run our bindPopup function (defined above) on
-        // each feature in the counties.oh.json geojson file.
-        L.geoJson(data, {
-            onEachFeature: bindPopup
-        }).addTo(countiesLayer);
+        // Since Leaflet already knows how to render GeoJSON, we won't have to
+        // do any processing to the data.
+        L.geoJson(data).addTo(countiesLayer);
 
         // We can still zoom to the extents of a layer by using our old
         // FeatureGroup layer trick
         mymap.fitBounds(countiesLayer.getBounds());
-
     }); // $.get()
 }); // document.ready()
